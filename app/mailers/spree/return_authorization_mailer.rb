9600fa1c7201a2a1985237b0d96952a90936b610
Anurag Bhardwaj <anurag.bhardwaj@vinsol.com>
2016-05-23 16:37:04 +0530

    modified mailer

--- app/mailers/spree/return_authorization_mailer.rb
+++ app/mailers/spree/return_authorization_mailer.rb
@@ -9,12 +9,14 @@ module Spree
     def notify_return_initialization_to_user(return_auth_number)
       @return_authorization = Spree::ReturnAuthorization.find_by(number: return_auth_number)
       to_address = @return_authorization.order.user.email
-      mail(to: @return_authorization, from: from_address, subject: "ReturnAuthorization #{ return_auth_number } has been Initiated")
+      mail(to: @return_authorization.order.user.email, from: from_address, subject: "ReturnAuthorization #{ return_auth_number } has been Initiated")
     end
 
     private
       def admin_notification_address
-        (Spree::Config[:return_initiation_admin_mail_address]).split(',').collect(&:strip)
+        addresses = Spree::Config[:return_initiation_admin_mail_address]
+        (addresses = (addresses).split(',').collect(&:strip)) if addresses.is_a?(Array)
+        addresses
       end
   end
 end
--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,4 +1,3 @@
 Spree::AppConfiguration.class_eval do
   preference :return_initiation_admin_mail_address, :string
-  preference :return_order_days, :integer
 end

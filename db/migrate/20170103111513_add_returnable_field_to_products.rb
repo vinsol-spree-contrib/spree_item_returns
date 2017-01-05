25d8bcc7bb6935899be6544afd66d3ec79ce6e7c
PareshGupta <paresh@vinsol.com>
2017-01-05 19:08:08 +0530

    change returnable product default to false

--- db/migrate/20170103111513_add_returnable_field_to_products.rb
+++ db/migrate/20170103111513_add_returnable_field_to_products.rb
@@ -1,5 +1,5 @@
 class AddReturnableFieldToProducts < ActiveRecord::Migration
   def change
-    add_column :spree_products, :returnable, :boolean, default: true
+    add_column :spree_products, :returnable, :boolean, default: false
   end
 end

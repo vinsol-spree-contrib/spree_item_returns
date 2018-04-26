34f10a8469eac33bb8c8fd6b5306a5a6b4c46fcd
Himanshu <himanshumishra3@gmail.com>
2018-04-26 16:10:05 +0530

    appraise and fix for 3-5 and master

--- app/overrides/spree/return_authorization/user_return_authorization_initailization_button.rb
+++ app/overrides/spree/return_authorization/user_return_authorization_initailization_button.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   virtual_path: 'spree/orders/show',
   name: 'user_return_authorization_initailization_button',
-  insert_bottom: "#order_summary legend",
+  insert_bottom: "#order_summary h1",
   text: "
         <% if @order.shipped? && @order.has_returnable_products? && @order.has_returnable_line_items? %>
           <%= link_to(spree.new_order_return_authorization_path(@order), class: 'btn btn-primary pull-right') do %>
